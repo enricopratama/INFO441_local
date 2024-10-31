f1b54879fa8f9e943d4516494195ccad221206df
Enrico Pratama <enricopratama10@gmail.com>
2024-10-31 10:48:00 -0700

    Added Sessions

---
+++ lecture/lecture_09_sessions_auth/demo-1-session-begin/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import sessions from 'express-session';
+import usersRouter from './routes/users.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+app.use(sessions({
+    secret: "this is some secret key that should be stored in a .env file 09291nji322081",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay }, // how long I want the session to last
+    resave: false   // save the session even if it wasn't modified
+}))
+
+app.use('/users', usersRouter);
+
+export default app;
---
+++ lecture/lecture_09_sessions_auth/demo-1-session-begin/routes/users.js
@@ -0,0 +1,33 @@
+import express from 'express';
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  if (req.session.userid) {
+    res.send(`You are logged in as ${req.session.userid}`);
+  } else {
+    res.status(403).send("You are not logged in");
+  }
+});
+
+router.post("/login", async(req, res) => {
+  // TODO: add login logic in DB here later (DB HERE, password should be hashed)
+  if (req.body.username == "kylethayer" && req.body.password == "asdasd"){ // For testing
+    req.session.userid = "kylethayer"; 
+    res.send("you logged in!");
+    console.log("Login successful");
+  } else if (req.body.username == "anotheruser" && req.body.password == "asdasd"){ // For testing
+    req.session.userid = "anotheruser"; 
+    res.send("you logged in!");
+    console.log("Login successful");
+  } else {
+    res.status(403).send("Invalid login");
+  }
+})
+
+router.post("/logout", async(req, res) => {
+  req.session.destroy();
+  res.send("You logged out successfully");
+})
+
+export default router;
