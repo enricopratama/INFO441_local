4ad5b3683da3ad0f717c3b649e35b35e957fd5c9
Enrico Pratama <enricopratama10@gmail.com>
2024-11-08 22:43:13 -0800

    added microsoft user authentication

--- lecture/lecture_11_user_actions_part_2/demo-1-playlists-begin/routes/api/v1/apiv1.js
+++ lecture/lecture_11_user_actions_part_2/demo-1-playlists-begin/routes/api/v1/apiv1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express'
 let router = express.Router()
 
@@ -9,14 +8,3 @@ router.use("/users", usersRouter)
 router.use('/playlists', playlistRouter);
 
 export default router
-||||||| 1ea6190
-=======
-import express from 'express'
-let router = express.Router()
-
-import usersRouter from "./controllers/users.js"
-
-router.use("/users", usersRouter)
-
-export default router
->>>>>>> 9a62a48d4693cd43b7923e7c728d54c98eb6129c
